c55377837e5474b67b11e9fcd9048d81c23ab102
Edmond Wu <edmond.wu@velocitylight.net>
2021-09-03 18:17:19 +0800

    Update image croper

--- src/screens/AppScreen/components/CropDialog.js
+++ src/screens/AppScreen/components/CropDialog.js
@@ -1,52 +1,47 @@
-import React, {useState, useCallback} from 'react';
-import Cropper from 'react-easy-crop'
-import { getCroppedImg } from '../../../utils/imageUtils';
-
+import React, { useState, useCallback } from "react";
+import Cropper from "react-easy-crop";
+import { getCroppedImg } from "../../../utils/imageUtils";
 
 const CropDialog = ({ pic, callback, onConfirm }) => {
-  const [crop, setCrop] = useState({ x: 0, y: 0 })
-  const [zoom, setZoom] = useState(1)
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [cropSize, setCropSize] = useState({ width: 0, height: 0 });
+  const [zoom, setZoom] = useState(1);
 
   const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
-    console.log(croppedArea, croppedAreaPixels)
-    setCrop(croppedAreaPixels);
+    console.log(croppedArea, croppedAreaPixels);
+    setCropSize(croppedAreaPixels);
   }, []);
 
-  
   const showCroppedImage = useCallback(async () => {
     onConfirm();
     try {
-     await getCroppedImg(
-        pic,
-        crop,
-        callback
-      )     
+      await getCroppedImg(pic, { ...crop, ...cropSize}, callback);
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
-  }, [crop])
+  }, [crop]);
 
   return (
     <div>
- <Cropper
-      image={pic}
-      crop={crop}
-      zoom={zoom}
-      aspect={3 / 4}
-      onCropChange={setCrop}
-      onCropComplete={onCropComplete}
-      onZoomChange={setZoom}
-      style={{
-        containerStyle: {
-          position: 'relative',
-          width: 400,
-          height: 400
-        }
-      }}
-    />
-    <button onClick={showCroppedImage}>確認上傳</button>
+      <Cropper
+        image={pic}
+        crop={crop}
+        zoom={zoom}
+        aspect={3 / 4}
+        onCropChange={setCrop}
+        onCropComplete={onCropComplete}
+        onZoomChange={setZoom}
+        style={{
+          containerStyle: {
+            position: "relative",
+            width: 400,
+            height: 400,
+          },
+        }}
+      />
+      <button onClick={showCroppedImage}>確認上傳</button>
     </div>
-  )
-}
+  );
+};
 
 export default CropDialog;
